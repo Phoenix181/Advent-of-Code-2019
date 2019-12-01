58c4ebab72c6c2f0216c4a0213199a23aff06652
Phoenix181 <40687700+Phoenix181@users.noreply.github.com>
2019-12-01 16:07:35 +0530

    Finished Day1

---
+++ Day1/day1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+//part1
+int fuelReq(int mass) {
+    return mass / 3 - 2;
+}
+
+//part2
+int netFuelReq(int mass) {
+    int fuel = fuelReq(mass);
+    
+    if(fuel <= 0)
+        return 0;
+    
+    return fuel + netFuelReq(fuel);
+}
+
+int main() {
+    std::vector<int> masses;
+    int total = 0, input;
+
+    std::cin >> input;
+    while(input > 0) {
+        masses.push_back(input);
+        std::cin >> input;
+    }
+
+    for (auto mass : masses) {
+        total += netFuelReq(mass);
+    }
+    
+    std::cout << total;
+}
